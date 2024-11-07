dc91cf9ebc45a874ece606de549999b0e4e1904d
Jessica <jesss8692@hotmail.com>
2024-11-07 21:13:25 +0100

    Add tests for delete posts

--- blog-backend/tests/posts.test.js
+++ blog-backend/tests/posts.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest')
+const { response } = require('../app')
 const app =  require('../app')
 
 describe('Blog post API',() => {
@@ -17,5 +18,24 @@ describe('Blog post API',() => {
         .get('/api/posts')
 
         expect(response.statusCode).toBe(200)
+        expect(Array.isArray(response.body)).toBe(true); 
+    })
+
+    it('should delete the post', async () => {
+        // create a post
+        const newPost = await request(app)
+        .post('/api/posts')
+        .send({title:'Delete post', content:'This post is being deleted'})
+
+        // create the id
+        const postId = newPost.body.id 
+
+        // delete the post
+        const deleteResponse =  await request(app).delete(`/api/posts/${postId}`)
+        expect(deleteResponse.statusCode).toBe(204)
+
+        const getResponse = await request(app).get(`/api/posts/${postId}`)
+        expect(getResponse.statusCode).toBe(404)
+        
     })
 })
